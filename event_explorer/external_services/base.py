343ec60826aa8edc048e19825e041020e088f098
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-05-24 13:25:03 -0400

    added classes for eventbrite events and attendees

---
+++ event_explorer/external_services/base.py
@@ -0,0 +1,94 @@
+import requests
+
+
+class ExternalService(requests.Session):
+    """Base class for making API calls from across REST APIs."""
+
+    base_url = None
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def request(self, method, url, *args, **kwargs):
+        url = self._create_url(url)
+        return super().request(method, url, *args, **kwargs)
+
+    def _create_url(self, url):
+        if not self.base_url:
+            raise ValueError("Base URL for the external service has not been set.")
+        return f"{self.base_url}{url}"
+
+
+class Event:
+    """Base class for normalizing events from across sources."""
+
+    source = None
+    attendees = None
+
+    def __init__(self, event):
+        self.event = event
+
+    def get_id(self):
+        raise NotImplementedError
+
+    def get_name(self):
+        raise NotImplementedError
+
+    def get_source(self):
+        return self.source
+
+    def get_time(self):
+        raise NotImplementedError
+
+    def get_description(self):
+        raise NotImplementedError
+
+    def get_attendees(self):
+        raise NotImplementedError
+
+
+class Attendee:
+    """Base class for normalizing attendee information from across sources."""
+
+    source = None
+
+    def __init__(self, attendee):
+        self.attendee = attendee
+
+    def get_first_name(self):
+        raise NotImplementedError
+
+    def get_last_name(self):
+        raise NotImplementedError
+
+    def get_email(self):
+        raise NotImplementedError
+
+    def get_source(self):
+        return self.source
+
+
+def get(url, session):
+    """Makes a call to the Eventbrite API using.
+
+    Parameters
+    ----------
+    url : str
+        The endpoint to call. Do not include the base url.
+    session : request.Session
+        The external service to use for the API call
+
+    Returns
+    -------
+    response : requests.Response
+        The response from the Eventbrite API
+    """
+    response = session.get(url)
+    if response.status_code == 200:
+        return response
+    else:
+        status_code = response.status_code
+        raise ValueError(f"{str(session)} API failed. Status code: {status_code}")
--- event_explorer/external_services/eventbrite.py
+++ event_explorer/external_services/eventbrite.py
@@ -1,26 +1,77 @@
+import datetime
 import os
+
 import requests
 
+from event_explorer.external_services.base import Attendee, Event, ExternalService, get
 
-class Eventbrite(requests.Session):
+
+class Eventbrite(ExternalService):
     base_url = "https://www.eventbriteapi.com/v3"
 
     def __init__(self):
         super().__init__()
         self._set_header_token()
 
-    def request(self, method, url, *args, **kwargs):
-        """Send the request after generating the complete URL."""
-        url = self.create_url(url)
-        print(url)
-        return super().request(method, url, *args, **kwargs)
-
-    def create_url(self, url):
-        """Create the URL based off this partial path."""
-        return f"{self.base_url}{url}"
-
     def _set_header_token(self):
         token = os.environ.get("EVENTBRITE_TOKEN", None)
         if not token:
             raise ValueError("EVENTBRITE_TOKEN environmental variable not set")
         self.headers.update({"Authorization": f"Bearer {token}"})
+
+
+class EventbriteEvent(Event):
+    """Class for representing Eventbrite events that are fetched from the API."""
+
+    source = "Eventbrite"
+
+    @classmethod
+    def from_dict(cls, event):
+        return cls(event)
+
+    @classmethod
+    def from_id(cls, event_id):
+        response = get(f"/events/{event_id}", Eventbrite())
+        return cls(response.json())
+
+    def get_id(self):
+        return self.event["id"]
+
+    def get_name(self):
+        return self.event["name"]["text"]
+
+    def get_time(self):
+        time = self.event["start"]["local"]
+        return datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S")
+
+    def get_description(self):
+        return self.event["description"]["text"]
+
+    def get_attendees(self):
+        if self.attendees is None:
+            event_id = self.get_id()
+            response = get(f"/events/{event_id}/attendees", Eventbrite())
+            self.attendees = [
+                EventbriteAttendee.from_dict(attendee)
+                for attendee in response.json()["attendees"]
+            ]
+        return self.attendees
+
+
+class EventbriteAttendee(Attendee):
+    """Class for representing attendees of Eventbrite events."""
+
+    source = "Eventbrite"
+
+    @classmethod
+    def from_dict(cls, attendee):
+        return cls(attendee)
+
+    def get_first_name(self):
+        return self.attendee["profile"]["first_name"]
+
+    def get_last_name(self):
+        return self.attendee["profile"]["last_name"]
+
+    def get_email(self):
+        return self.attendee["profile"]["email"]
