f31b6c114872103982c2b34f0bdfcfbbf466cbb2
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-05-31 13:41:25 -0400

    added class for pulling zoom metadata

---
+++ event_explorer/external_services/zoom.py
@@ -0,0 +1,94 @@
+import datetime
+from dateutil import tz
+import os
+import time
+
+import jwt
+
+from event_explorer.external_services.base import Attendee, Event, ExternalService, get
+
+
+class Zoom(ExternalService):
+    base_url = "https://api.zoom.us/v2"
+
+    def __init__(self):
+        super().__init__()
+        self._set_header_token()
+
+    def _set_header_token(self):
+        key = os.environ.get("ZOOM_API_KEY", None)
+        secret = os.environ.get("ZOOM_API_SECRET", None)
+        if not all([key, secret]):
+            raise ValueError(
+                "ZOOM_API_KEY or ZOOM_API_SECRET environmental " "variable not set"
+            )
+        token = generate_jwt(key, secret)
+        self.headers.update({"Authorization": f"Bearer {token}"})
+
+
+class ZoomEvent(Event):
+    """Class for representing Zoom events that are fetched from the API."""
+
+    source = "Zoom"
+
+    @classmethod
+    def from_dict(cls, event):
+        return cls(event)
+
+    @classmethod
+    def from_id(cls, event_id):
+        response = get(f"/meetings/{event_id}", Zoom())
+        return cls(response.json())
+
+    def get_id(self):
+        return self.event["uuid"]
+
+    def get_name(self):
+        return self.event["topic"]
+
+    def get_time(self):
+        time = self.event["start_time"]
+        from_zone = tz.gettz("UTC")
+        time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
+        time = time.replace(tzinfo=from_zone)
+
+        to_zone = tz.gettz(self.event["timezone"])
+        return time.astimezone(to_zone)
+
+    def get_description(self):
+        # TODO: Can we get the description anywhere in the Zoom API?
+        return None
+
+    def get_attendees(self):
+        if self.attendees is None:
+            event_id = self.get_id()
+            response = get(f"/meetings/{event_id}/registrants", Zoom())
+            self.attendees = [
+                ZoomAttendee.from_dict(attendee)
+                for attendee in response.json()["attendees"]
+            ]
+        return self.attendees
+
+
+def generate_jwt(key, secret):
+    """Generates a JSON web token, which is used to authorize requests
+    to the Zoom API:
+
+    Parameters
+    ----------
+    key : str
+        The Zoom API key for the account
+    secret : str
+        The Zoom API secret for the account
+
+    Returns
+    -------
+    token : str
+        The token to use in API requests
+    """
+    header = {"alg": "HS256", "typ": "JWT"}
+
+    payload = {"iss": key, "exp": int(time.time() + 3600)}
+
+    token = jwt.encode(payload, secret, algorithm="HS256", headers=header)
+    return token.decode("utf-8")
---
+++ test_event_explorer/test_external_services/test_zoom.py
@@ -0,0 +1,127 @@
+import datetime
+from dateutil import tz
+import os
+
+import pytest
+
+import event_explorer.external_services.zoom as zoom_service
+
+
+class MockResponse:
+    def __init__(self, response):
+        self.response = response
+        self.status_code = 200
+
+    def json(self):
+        return self.response
+
+
+TEST_EVENT = {
+    "uuid": "12345",
+    "topic": "Dogs.com Info Session",
+    "start_time": "2020-04-11T03:04:00Z",
+    "timezone": "America/New_York",
+}
+
+
+TEST_ATTENDEES = {
+    "attendees": [
+        {
+            "profile": {
+                "first_name": "Tiki",
+                "last_name": "Robinson",
+                "email": "tiki@robinson.io",
+            }
+        }
+    ]
+}
+
+
+def test_zoom_gets_token_from_eviron(monkeypatch):
+    monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: "TEST_TOKEN")
+    zoom = zoom_service.Zoom()
+    assert zoom.headers["Authorization"].startswith("Bearer")
+
+
+def test_zoom_raises_error_with_no_token(monkeypatch):
+    monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: None)
+    with pytest.raises(ValueError):
+        zoom = zoom_service.Zoom()
+
+
+def test_zoom_loads_from_dict():
+    event = zoom_service.ZoomEvent.from_dict(TEST_EVENT)
+    assert (
+        event.get_id(),
+        event.get_name(),
+        event.get_time(),
+        event.get_description(),
+    ) == (
+        TEST_EVENT["uuid"],
+        TEST_EVENT["topic"],
+        datetime.datetime(2020, 4, 10, 23, 4, tzinfo=tz.gettz("America/New_York")),
+        None,
+    )
+
+
+# def test_zoom_loads_from_id(monkeypatch):
+#     monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: "TEST_TOKEN")
+#     monkeypatch.setattr(
+#         zoom_service, "get", lambda url, session: MockResponse(TEST_EVENT)
+#     )
+#     event = zoom_service.ZoomEvent.from_id("12345")
+#     assert (
+#         event.get_id(),
+#         event.get_name(),
+#         event.get_time(),
+#         event.get_description(),
+#     ) == (
+#         TEST_EVENT["uuid"],
+#         TEST_EVENT["topic"],
+#         datetime.datetime(2020, 4, 10, 23, 4, tzinfo=tz.gettz("America/New_York"),
+#         None,
+#     )
+
+
+# def test_zoom_event_loads_attendees(monkeypatch):
+#     monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: "TEST_TOKEN")
+#     monkeypatch.setattr(
+#         zoom_service, "get", lambda url, session: MockResponse(TEST_ATTENDEES)
+#     )
+#     event = zoom_service.ZoomEvent.from_dict(TEST_EVENT)
+#     attendees = event.get_attendees()
+#     assert (
+#         attendees[0].get_first_name(),
+#         attendees[0].get_last_name(),
+#         attendees[0].get_email(),
+#     ) == ("Tiki", "Robinson", "tiki@robinson.io")
+#
+#
+# def test_load_zoom(monkeypatch):
+#     def mock_get(url):
+#         if "attendees" in url:
+#             response = {"attendees": TEST_ATTENDEES}
+#         else:
+#             response = {
+#                 "events": [TEST_EVENT, TEST_EVENT, TEST_EVENT],
+#                 "pagination": {
+#                     "continuation": "abc",
+#                     "has_more_items": "continuation" not in url,
+#                 },
+#             }
+#         return MockResponse(response)
+#
+#     zoom_service.Zoom.__init__ = lambda self: None
+#     zoom_service.Zoom.get = lambda self, url: mock_get(url)
+#
+#     monkeypatch.setattr(zoom_service, "load_event", lambda *args, **kwargs: None)
+#     monkeypatch.setattr(
+#         zoom_service, "delete_event", lambda *args, **kwargs: None
+#     )
+#     monkeypatch.setattr(
+#         zoom_service, "load_attendee", lambda *args, **kwargs: None
+#     )
+#     monkeypatch.setattr(
+#         zoom_service, "delete_event_attendees", lambda *args, **kwargs: None
+#     )
+#     zoom_service.load_zoom()
