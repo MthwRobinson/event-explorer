f12d9ead96bd0c9bc1c1b513cea18f4feed7f097
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-06-16 20:45:19 -0400

    reading in rds user from envrionment

--- event_explorer/database/connection.py
+++ event_explorer/database/connection.py
@@ -6,4 +6,5 @@ def connect():
     host = os.environ.get("FIDDLER_RDS", None)
     if not host:
         raise ValueError("FIDDLER_RDS environmental variable is not defined.")
-    return psycopg2.connect(user="master", dbname="event_explorer", host=host)
+    rds_user = os.environ.get("RDS_USER", "master")
+    return psycopg2.connect(user=rds_user, dbname="event_explorer", host=host)
--- test_event_explorer/test_database/test_connection.py
+++ test_event_explorer/test_database/test_connection.py
@@ -7,13 +7,27 @@ import event_explorer.database.connection as connection
 
 
 def test_connection_works_with_env(monkeypatch):
-    monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: "test_host")
-    monkeypatch.setattr(psycopg2, "connect", lambda user, host, dbname: host)
-    assert connection.connect() == "test_host"
+    current_environ = dict(os.environ)
+    os.environ["FIDDLER_RDS"] = "test_host"
+    os.environ["RDS_USER"] = "test_user"
+
+    monkeypatch.setattr(psycopg2, "connect", lambda user, dbname, host: (user, host))
+    assert connection.connect() == ("test_user", "test_host")
+
+    del os.environ["FIDDLER_RDS"]
+    del os.environ["RDS_USER"]
+
+    for variable, value in current_environ.items():
+        os.environ[variable] = value
 
 
 def test_connection_raises_error_with_no_env(monkeypatch):
-    monkeypatch.setattr(os.environ, "get", lambda *args, **kwargs: None)
+    fiddler_rds = os.environ.get("FIDDLER_RDS")
+    del os.environ["FIDDLER_RDS"]
+
     monkeypatch.setattr(psycopg2, "connect", lambda user, host, dbname: host)
     with pytest.raises(ValueError):
         connection.connect()
+
+    if fiddler_rds:
+        os.environ["FIDDLER_RDS"] = fiddler_rds
